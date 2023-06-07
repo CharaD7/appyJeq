6069a376fa884323eb76856fac6e6f2f3224afd4
CharaD7 <jijakahn6@gmail.com>
2023-06-07 20:58:29 +0000

    Added more eslint rules

--- MOBILE/journal-app/.eslintrc.js
+++ MOBILE/journal-app/.eslintrc.js
@@ -24,9 +24,35 @@ module.exports = {
     'jest',
     'import',
     'react',
+    'react-native',
     'simple-import-sort'
   ],
   'rules': {
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-unresolved': ['error', { 'commonjs': true }],
+    'import/no-extraneous-dependencies': 'error',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+        'alphabetize': {
+          'order': 'asc',
+          'caseInsensitive': true,
+        },
+      },
+    ],
+    'no-extra-semi': ['error'],
+    'sort-imports': ['warn', { 'ignoreCase': true, 'ignoreDeclarationSort': true }],
+    '@typescript-eslint/no-use-before-define': 'warn',
+    'react/jsx-filename-extension': [
+      2,
+      { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'react-native/no-color-literals': 'off',
     'indent': [
       'error',
       2
@@ -42,6 +68,6 @@ module.exports = {
     'semi': [
       'error',
       'always'
-    ]
-  }
+    ],
+  },
 };
